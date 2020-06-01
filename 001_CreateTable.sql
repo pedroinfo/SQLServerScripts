23ad0e256e8d9d8cca8e9779fb14e83126d02861
Pedro <pedrodxavier@hotmail.com>
2020-06-01 15:13:32 -0300

    Add project files.

---
+++ 001_CreateTable.sql
@@ -0,0 +1,15 @@
+﻿USE TEST
+
+GO
+
+-- CREATE TABLE 
+
+-- A simple example of creating tables
+
+CREATE TABLE [TEST].[dbo].[Product](
+	[ProductId] [int] PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](100) NOT NULL,
+	[Price] [money] NOT NULL,
+	[CreatedAt] [datetime] NOT NULL,
+	[Active] [bit] NOT NULL
+);
